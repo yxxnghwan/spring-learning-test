c1687d0cbd5cc7c60e6a36d527abd58c2e0d74c8
yxxnghwan <younghwan960@gmail.com>
2022-04-28 19:55:55 +0900

    feat: spring mvc config 학습테스트

--- src/main/java/nextstep/helloworld/mvcconfig/config/WebMvcConfiguration.java
+++ src/main/java/nextstep/helloworld/mvcconfig/config/WebMvcConfiguration.java
@@ -1,5 +1,7 @@
 package nextstep.helloworld.mvcconfig.config;
 
+import nextstep.helloworld.mvcconfig.ui.AuthenticationPrincipalArgumentResolver;
+import nextstep.helloworld.mvcconfig.ui.LoginInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -17,6 +19,7 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
      */
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("hello");
     }
 
     /**
@@ -26,6 +29,7 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
      */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/admin/**");
     }
 
     /**
@@ -35,5 +39,6 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
      */
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new AuthenticationPrincipalArgumentResolver());
     }
 }
