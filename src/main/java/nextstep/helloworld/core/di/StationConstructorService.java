5c0ef45f7e64ba4713406fcf3fc236b17bf9d928
yxxnghwan <younghwan960@gmail.com>
2022-04-28 13:47:19 +0900

    feat: di, component scan 학습테스트

--- src/main/java/nextstep/helloworld/core/di/StationConstructorService.java
+++ src/main/java/nextstep/helloworld/core/di/StationConstructorService.java
@@ -1,7 +1,15 @@
 package nextstep.helloworld.core.di;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class StationConstructorService {
-    private StationRepository stationRepository;
+    private final StationRepository stationRepository;
+
+    public StationConstructorService(final StationRepository stationRepository) {
+        this.stationRepository = stationRepository;
+    }
 
     public String sayHi() {
         return stationRepository.sayHi();
--- src/main/java/nextstep/helloworld/core/di/StationSetterService.java
+++ src/main/java/nextstep/helloworld/core/di/StationSetterService.java
@@ -1,9 +1,18 @@
 package nextstep.helloworld.core.di;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class StationSetterService {
     private StationRepository stationRepository;
 
     public String sayHi() {
         return stationRepository.sayHi();
+    }
+
+    @Autowired
+    public void setStationRepository(final StationRepository stationRepository) {
+        this.stationRepository = stationRepository;
     }
 }
