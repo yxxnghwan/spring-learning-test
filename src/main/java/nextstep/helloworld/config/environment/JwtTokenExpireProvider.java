8aed5e8f142dd017d1b0425d27a8769bfc978e51
yxxnghwan <younghwan960@gmail.com>
2022-04-28 16:46:36 +0900

    feat: spring config 학습테스트

--- src/main/java/nextstep/helloworld/config/environment/JwtTokenExpireProvider.java
+++ src/main/java/nextstep/helloworld/config/environment/JwtTokenExpireProvider.java
@@ -1,5 +1,8 @@
 package nextstep.helloworld.config.environment;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 // TODO: 컴포넌트 스캔을 통한 빈 등록
 public class JwtTokenExpireProvider {
     // TODO: application.properties의 security-jwt-token-expire-length 값을 활용하여 validityInMilliseconds값 초기화 하기
--- src/main/java/nextstep/helloworld/config/environment/ValueConfig.java
+++ src/main/java/nextstep/helloworld/config/environment/ValueConfig.java
@@ -1,7 +1,30 @@
 package nextstep.helloworld.config.environment;
 
+import java.util.Objects;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
 // TODO: Java-based Configuration을 하기 위한 클래스로 지정하기
 // TODO: application.properties 파일을 활용하기 위한 설정 추가하기
 // TODO: nextstep.helloworld.core.environment 내에 있는 스프링 빈을 스캔하기
+@Configuration
+@PropertySource("classpath:/application.properties")
 public class ValueConfig {
+
+    private final Environment env;
+
+    public ValueConfig(final Environment env) {
+        this.env = env;
+    }
+
+    @Bean
+    public JwtTokenExpireProvider jwtTokenExpireProvider() {
+        return new JwtTokenExpireProvider(
+                Long.parseLong(
+                        Objects.requireNonNull(env.getProperty("security-jwt-token-expire-length"))
+                )
+        );
+    }
 }
